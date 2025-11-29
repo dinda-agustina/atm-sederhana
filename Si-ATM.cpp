0294fb98e5b9ccfe884580848f4f8ac33aba1f83
dinda-agustina <dindaagustinahidayani@gmail.com>
2025-11-29 23:14:48 +0700

    update fungsi login

--- Si-ATM.cpp
+++ Si-ATM.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 using namespace std;
 
+// Fungsi Login 
+bool loginPIN(int pinBenar) {
+    int pinInput;
+    int kesempatan = 3;
+
+    while (kesempatan > 0) {
+        cout << "Masukkan PIN: ";
+        cin >> pinInput;
+
+        if (pinInput == pinBenar) {
+            cout << "Login Berhasil!\n" << endl;
+            return true;
+        } else {
+            kesempatan--;
+            cout << "PIN Salah! Coba Lagi, Sisa Kesempatan: " << kesempatan << endl;
+        }
+    }
+
+    cout << "Akun Terblokir!" << endl;
+    return false;
+}
+
 int main(){
-    
+    int pinATM = 2025;
     return 0;
 }
+
